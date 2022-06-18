843783caa1c435320b4d29346740536f98de4180
Juank1965 <juancarlosbastidasq@gmail.com>
2022-06-18 17:47:27 -0500

    Iniciando proceso de deployment

--- client/src/components/navBarMain/NavBarMain.jsx
+++ client/src/components/navBarMain/NavBarMain.jsx
@@ -17,10 +17,22 @@ function NavBarMain() {
         <h1 className={style.titulo}>Videogames App</h1>
       </div>
       <div className={style.menu}>
-        <NavLink to="/" className={style.link}>
+        <NavLink
+          to="/"
+          className={style.boton}
+          style={({ isActive }) => {
+            return { color: isActive ? "#c80a50" : "white" };
+          }}
+        >
           <h3>INICIO</h3>
         </NavLink>
-        <NavLink to="/home" className={style.link}>
+        <NavLink
+          to="/home"
+          className={style.boton}
+          style={({ isActive }) => {
+            return { color: isActive ? "#c80a50" : "white" };
+          }}
+        >
           <h3>GAMES</h3>
         </NavLink>
       </div>
